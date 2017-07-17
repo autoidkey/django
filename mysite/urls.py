7fa6d21e5ad7166761b07a98472680b581c72b61
yuto <aaiukrw@gmail.com>
2017-07-18 00:43:14 +0900

    My Django app4

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
     #url(r'^admin/', admin.site.urls),
-    #url(r'', include('blog.urls')), #add
+    url(r'', include('blog.urls')), #add
 ]
