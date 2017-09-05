429ae5b6f3baeea55746091d10b5dd68768c7081
yuto <aaiukrw@gmail.com>
2017-09-05 15:03:20 +0900

    add admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, My_post
 
 admin.site.register(Post)
+admin.site.register(My_post)
 '''
 from django.contrib import admin
 from .models import Post
